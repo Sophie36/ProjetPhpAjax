8a44a1a541292c4d0a2a280d5c3dea99ba1d5859
slemaire <slemaire@E117-03.CDF36.local>
2017-02-15 15:41:19 +0100

    fonction AJAX qu'il faut se servir à chaque fois dans validation.js

--- inscription.php
+++ inscription.php
@@ -1,11 +1,46 @@
 <?php
+require_once('include/connexion.php');
+require_once('include/infoConnexion.php');
+require_once('include/executeRequete.php');
+
+$cnx=connexion(UTILISATEUR,MOTDEPASSE,SERVER,BASEDEDONNEES);
+if(isset($_SESSION['login'])){
+    session_start();//sans cette session le html ne marche pas
+    $_SESSION=array();
+    session_destroy();
+}
+if(isset($_POST['valider'])){
+   $nom=$_POST['nom'];
+   $prenom=$_POST['prenom'];
+   $login=$_POST['login'];
+   $pw=$_POST['pw'];
+   $droite="tk!@";
+   $gauche="ar30b%";
+   if($login==""||$pw==""){
+       echo'Saisissez votre login et votre mot de passe';
+   }else{
+       $sql="SELECT login FROM utilisateurs WHERE('$login')";
+       $idRequete=executeRequete($cnx,$sql);
+       if($idRequete->rowCount()==1){
+           echo'login existant';
+       }else{
+           $jeton=hash('ripemd128',"$gauche.$pw.$droite");
+           $sql="INSERT INTO utilisateurs VALUES('$prenom','$nom','$login','$jeton')";
+           $idRequete=executeRequete($cnx,$sql);
+           header('location:index.php');
+           
+       }
+    }
+}
 ?>
 <form method="post" action="inscription.php">        
                 Nom: <input type="text" name="nom" value="" placeholder="Nom" size="15" maxlength="10"></br>
                 </br>
                 Prenom: <input type="text" name="prenom" value="" placeholder="Prénom" size="15" maxlength="10"></br>
                 </br>
-                Login: <input type='text' name='f_login' value='' size="15" maxlength="10"></br>
+                Login: <input type='text' name='login' value=''size="15" maxlength="10" onBlur="verifUtilisateur(this);"><span id='info'></span></br>
                 </br>
-                Mot de passe: <input type='password' name='f_pw' value='' size="15" maxlength="10"></br>
+                Mot de passe: <input type='password' name='pw' value='' size="15" maxlength="10"></br>
+                </br>
+                <input type='submit' name='valider' value='Valider'>
 </form> 
--- validation.js
+++ validation.js
@@ -1,8 +1,49 @@
+//a garder pour à chaque fois que l'on a besoin d AJAX
 function verifUtilisateur(login){
     $i=document.getElementById('info');
     if(login.value==""){
         $i.innerHTML='';
+    }else{
+        $i.innerHTML="Le login est pas bon";
     }
-   }
+    obAjax=new creationObjetXMLHttpRequest();
+    args='login='+login.value;
+    obAjax.open('POST','controle.php',true);
+    obAjax.setRequestHeader("content-type","application/x-www-form-urlencoded");
+    obAjax.setRequestHeader("content-length",args.length);
+    obAjax.onreadysttechange=traitementResultat;//nom de la fonction
+    obAjax.send(args);
+}
+
+function creationObjetXMLHttpRequest(){
+    try{
+        var requete=new XMLHttpRequest();
+    }catch(e1){
+            try{
+                var requete=new ActiveXobject("Msxml2.XMLHTTP");
+            }
+            catch(e2){
+                try{
+                    requete= new ActiveXobject("Microsoft.XMLHTTP");
+                }
+                catch(e3){
+                    return False;
+                }
+            }
+        }
+    return requete;
+}
+//fin verifUtilisateur()
+
+function traitementResultat(){
+    if(this.readystate==4){
+        if(this.status==200){
+            if(this.reponseText!=""){
+                $i.innerHTML=this.reponseText;
+            }
+        }
+    }
+}
+
 
 
